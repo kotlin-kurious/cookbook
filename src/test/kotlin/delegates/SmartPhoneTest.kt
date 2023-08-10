9b9b02a75e389d9cbe8e8752fea40475309bab1f
arisromil <arisromil@yahoo.com>
2023-08-10 08:41:38 -0600

    delegation `by`

---
+++ src/test/kotlin/delegates/SmartPhoneTest.kt
@@ -0,0 +1,18 @@
+package delegates
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+class SmartPhoneTest {
+    private val smartPhone: SmartPhone = SmartPhone()
+
+    @Test
+    fun `Dialing delegates to internal phone`() {
+        assertEquals("Dialing 303-1234...", smartPhone.dial("303-1234"))
+    }
+
+    @Test
+    fun `Taking picture delegates to internal camera`() {
+        assertEquals("Taking picture...", smartPhone.takePicture())
+    }
+}
