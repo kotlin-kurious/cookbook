3dff9f78f19abb1d5956850f7e22cffad55cd002
arisromil <arisromil@yahoo.com>
2023-08-20 00:04:40 -0600

    spring integration config setting
    Add the Kotlin JVM plug-in to the project
    Requires the Kotlin Spring plug-in
    Required if the project source code is written in Kotlin
    Supports nullability annotations associated with JSR-305

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,27 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-
+    id("org.springframework.boot") version "2.1.8.RELEASE"
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
     kotlin("jvm") version "1.9.0";
     application
+    kotlin("plugin.spring") version "1.2.71"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-
+    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.5.2")
-
     testImplementation(kotlin("test"))
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.test {
@@ -25,7 +29,10 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
 }
 
 application {
